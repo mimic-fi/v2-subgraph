24a415d0eedc13bbe12a7469f41f70a7d14c923c
Facu Spagnuolo <facundo_spagnuolo@icloud.com>
2022-10-23 21:53:23 -0300

    complete wallet handlers

--- src/ERC20.ts
+++ src/ERC20.ts
@@ -3,7 +3,26 @@ import { Address, log } from '@graphprotocol/graph-ts'
 import { ERC20 as ERC20Entity } from '../types/schema'
 import { ERC20 as ERC20Contract } from '../types/Registry/ERC20'
 
+const NATIVE_TOKEN_ADDRESS = Address.fromString('0xeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee')
+
+export function loadOrCreateNativeToken(): ERC20Entity {
+  let id = NATIVE_TOKEN_ADDRESS.toHexString()
+  let erc20 = ERC20Entity.load(id)
+
+  if (erc20 === null) {
+    erc20 = new ERC20Entity(id)
+    erc20.name = 'Native Token'
+    erc20.symbol = 'NTK'
+    erc20.decimals = 18
+    erc20.save()
+  }
+
+  return erc20
+}
+
 export function loadOrCreateERC20(address: Address): ERC20Entity {
+  if (address.equals(NATIVE_TOKEN_ADDRESS)) return loadOrCreateNativeToken()
+
   let id = address.toHexString()
   let erc20 = ERC20Entity.load(id)
 
--- src/Wallet.ts
+++ src/Wallet.ts
@@ -1,7 +1,8 @@
 import {Address, BigInt, ethereum, log } from '@graphprotocol/graph-ts'
 
 import { Wallet as WalletContract } from '../types/templates/Wallet/Wallet'
-import { Balance, ERC20, FeeConfig, FeePaid, PrimitiveExecution, Wallet } from '../types/schema'
+import { Strategy as StrategyContract } from '../types/templates/Wallet/Strategy'
+import { Balance, ERC20, FeeConfig, FeePaid, PriceFeed, PrimitiveExecution, Wallet } from '../types/schema'
 import {
   Call,
   Collect,
@@ -25,7 +26,9 @@ import {
 } from '../types/templates/Wallet/Wallet'
 
 
-import { loadOrCreateERC20 } from './ERC20'
+const ZERO_ADDRESS = Address.fromString('0x0000000000000000000000000000000000000000')
+
+import { loadOrCreateERC20, loadOrCreateNativeToken } from './ERC20'
 import { processAuthorizedEvent, processUnauthorizedEvent } from './Permissions'
 
 export function handleCall(event: Call): void {
@@ -97,31 +100,201 @@ export function handleWithdraw(event: Withdraw): void {
 }
 
 export function handleWrap(event: Wrap): void {
+  let wallet = Wallet.load(event.address.toHexString())!
 
+  let tokenOut = loadOrCreateNativeToken()
+  let balanceOut = loadOrCreateBalance(wallet, tokenOut)
+  balanceOut.amount = balanceOut.amount.minus(event.params.wrapped)
+  balanceOut.save()
+
+  let tokenIn = ERC20.load(wallet.wrappedNativeToken)!
+  let balanceIn = loadOrCreateBalance(wallet, tokenIn)
+  balanceIn.amount = balanceIn.amount.plus(event.params.wrapped)
+  balanceIn.save()
+
+  let executionId = wallet.id + '/executions/' + getTransactionId(event)
+  let execution = new PrimitiveExecution(executionId)
+  execution.type = 'Wrap'
+  execution.vault = wallet.vault
+  execution.wallet = wallet.id
+  execution.tokenOut = tokenOut.id
+  execution.amountOut = event.params.wrapped
+  execution.tokenIn = tokenIn.id
+  execution.amountIn = event.params.wrapped
+  execution.data = event.params.data.toHexString()
+  execution.executedAt = event.block.timestamp
+  execution.transaction = event.transaction.hash.toHexString()
+  execution.save()
 }
 
 export function handleUnwrap(event: Unwrap): void {
+  let wallet = Wallet.load(event.address.toHexString())!
 
+  let tokenOut = ERC20.load(wallet.wrappedNativeToken)!
+  let balanceOut = loadOrCreateBalance(wallet, tokenOut)
+  balanceOut.amount = balanceOut.amount.minus(event.params.unwrapped)
+  balanceOut.save()
+
+  let tokenIn = loadOrCreateNativeToken()
+  let balanceIn = loadOrCreateBalance(wallet, tokenIn)
+  balanceIn.amount = balanceIn.amount.plus(event.params.unwrapped)
+  balanceIn.save()
+
+  let executionId = wallet.id + '/executions/' + getTransactionId(event)
+  let execution = new PrimitiveExecution(executionId)
+  execution.type = 'Unwrap'
+  execution.vault = wallet.vault
+  execution.wallet = wallet.id
+  execution.tokenOut = tokenOut.id
+  execution.amountOut = event.params.unwrapped
+  execution.tokenIn = tokenIn.id
+  execution.amountIn = event.params.unwrapped
+  execution.data = event.params.data.toHexString()
+  execution.executedAt = event.block.timestamp
+  execution.transaction = event.transaction.hash.toHexString()
+  execution.save()
 }
 
 export function handleClaim(event: Claim): void {
+  let wallet = Wallet.load(event.address.toHexString())!
+  let eventTokens = event.params.tokens
+  let eventAmounts = event.params.amounts
 
+  for (let i: i32 = 0; i < eventTokens.length; i++) {
+    let tokenIn = loadOrCreateNativeToken()
+    let balance = loadOrCreateBalance(wallet, tokenIn)
+    balance.amount = balance.amount.plus(eventAmounts[i])
+    balance.save()
+
+    let executionId = wallet.id + '/executions/' + getTransactionId(event) + '/' + i.toString()
+    let execution = new PrimitiveExecution(executionId)
+    execution.type = 'Claim'
+    execution.vault = wallet.vault
+    execution.wallet = wallet.id
+    execution.tokenIn = tokenIn.id
+    execution.amountIn = eventAmounts[i]
+    execution.data = event.params.data.toHexString()
+    execution.executedAt = event.block.timestamp
+    execution.transaction = event.transaction.hash.toHexString()
+    execution.save()
+  }
 }
 
 export function handleJoin(event: Join): void {
+  let wallet = Wallet.load(event.address.toHexString())!
+  let token = loadOrCreateERC20(getStrategyToken(event.params.strategy))
+  let balance = loadOrCreateBalance(wallet, token)
 
+  balance.amount = balance.amount.minus(event.params.invested)
+  balance.save()
+
+  let executionId = wallet.id + '/executions/' + getTransactionId(event)
+  let execution = new PrimitiveExecution(executionId)
+  execution.type = 'Join'
+  execution.vault = wallet.vault
+  execution.wallet = wallet.id
+  execution.tokenOut = token.id
+  execution.amountOut = event.params.invested
+  execution.data = event.params.data.toHexString()
+  execution.executedAt = event.block.timestamp
+  execution.transaction = event.transaction.hash.toHexString()
+  execution.save()
+
+  // TODO: handle liquidity pool tokens
 }
 
 export function handleExit(event: Exit): void {
+  let wallet = Wallet.load(event.address.toHexString())!
+  let token = loadOrCreateERC20(getStrategyToken(event.params.strategy))
+  let balance = loadOrCreateBalance(wallet, token)
 
+  balance.amount = balance.amount.plus(event.params.received)
+  balance.save()
+
+  let executionId = wallet.id + '/executions/' + getTransactionId(event)
+  let execution = new PrimitiveExecution(executionId)
+  execution.type = 'Exit'
+  execution.vault = wallet.vault
+  execution.wallet = wallet.id
+  execution.tokenIn = token.id
+  execution.amountIn = event.params.received
+  execution.data = event.params.data.toHexString()
+  execution.executedAt = event.block.timestamp
+  execution.transaction = event.transaction.hash.toHexString()
+  execution.save()
+
+  // TODO: handle liquidity pool tokens
+
+  if (!event.params.fee.isZero()) {
+    let feeId = wallet.id + '/paid-fees/' + getTransactionId(event)
+    let fee = new FeePaid(feeId)
+    fee.vault = wallet.vault
+    fee.wallet = wallet.id
+    fee.primitive = execution.id
+    fee.token = token.id
+    fee.amount = event.params.fee
+    fee.pct = getPerformanceFeePct(event.address)
+    fee.feeCollector = getFeeCollector(event.address)
+    fee.save()
+  }
 }
 
 export function handleSwap(event: Swap): void {
+  let wallet = Wallet.load(event.address.toHexString())!
 
+  // The token in for the swap is the token out in the wallet
+  let tokenOut = loadOrCreateERC20(event.params.tokenIn)
+  let balanceOut = loadOrCreateBalance(wallet, tokenOut)
+  balanceOut.amount = balanceOut.amount.minus(event.params.amountIn)
+  balanceOut.save()
+
+  // The token in for the swap is the token out in the wallet
+  let tokenIn = loadOrCreateERC20(event.params.tokenOut)
+  let balanceIn = loadOrCreateBalance(wallet, tokenIn)
+  balanceIn.amount = balanceIn.amount.plus(event.params.amountOut)
+  balanceIn.save()
+
+  let executionId = wallet.id + '/executions/' + getTransactionId(event)
+  let execution = new PrimitiveExecution(executionId)
+  execution.type = 'Swap'
+  execution.vault = wallet.vault
+  execution.wallet = wallet.id
+  execution.tokenIn = tokenIn.id
+  execution.amountIn = event.params.amountOut
+  execution.tokenOut = tokenOut.id
+  execution.amountOut = event.params.amountIn
+  execution.data = event.params.data.toHexString()
+  execution.executedAt = event.block.timestamp
+  execution.transaction = event.transaction.hash.toHexString()
+  execution.save()
+
+  // TODO: handle liquidity pool tokens
+
+  if (!event.params.fee.isZero()) {
+    let feeId = wallet.id + '/paid-fees/' + getTransactionId(event)
+    let fee = new FeePaid(feeId)
+    fee.vault = wallet.vault
+    fee.wallet = wallet.id
+    fee.primitive = execution.id
+    fee.token = tokenIn.id
+    fee.amount = event.params.fee
+    fee.pct = getSwapFeePct(event.address)
+    fee.feeCollector = getFeeCollector(event.address)
+    fee.save()
+  }
 }
 
 export function handleStrategySet(event: StrategySet): void {
+  let wallet = Wallet.load(event.address.toHexString())!
+  let strategies = wallet.strategies
 
+  let strategy = event.params.strategy.toHexString()
+  let index = strategies.indexOf(strategy)
+  if (event.params.allowed && index < 0) strategies.push(strategy)
+  else if (!event.params.allowed && index >= 0) strategies.splice(index, 1)
+
+  wallet.strategies = strategies
+  wallet.save()
 }
 
 export function handlePriceOracleSet(event: PriceOracleSet): void {
@@ -131,7 +304,14 @@ export function handlePriceOracleSet(event: PriceOracleSet): void {
 }
 
 export function handlePriceFeedSet(event: PriceFeedSet): void {
-
+  let wallet = Wallet.load(event.address.toHexString())!
+  let feedId = wallet.id + '/feeds/' + event.params.base.toHexString() + '/' + event.params.quote.toHexString()
+  let feed = new PriceFeed(feedId)
+  feed.base = event.params.base.toHexString()
+  feed.quote = event.params.quote.toHexString()
+  feed.vault = wallet.vault
+  feed.wallet = wallet.id
+  feed.save()
 }
 
 export function handleSwapConnectorSet(event: SwapConnectorSet): void {
@@ -258,6 +438,42 @@ function getWithdrawFeePct(address: Address): BigInt {
 
   log.warning('withdrawFee() call reverted for {}', [address.toHexString()])
   return BigInt.zero()
+}
+
+function getPerformanceFeePct(address: Address): BigInt {
+  let wallet = WalletContract.bind(address)
+  let performanceFeeCall = wallet.try_performanceFee()
+
+  if (!performanceFeeCall.reverted) {
+    return performanceFeeCall.value.value0
+  }
+
+  log.warning('performanceFee() call reverted for {}', [address.toHexString()])
+  return BigInt.zero()
+}
+
+function getSwapFeePct(address: Address): BigInt {
+  let wallet = WalletContract.bind(address)
+  let swapFeeCall = wallet.try_swapFee()
+
+  if (!swapFeeCall.reverted) {
+    return swapFeeCall.value.value0
+  }
+
+  log.warning('swapFee() call reverted for {}', [address.toHexString()])
+  return BigInt.zero()
+}
+
+function getStrategyToken(address: Address): Address {
+  let strategy = StrategyContract.bind(address)
+  let tokenCall = strategy.try_token()
+
+  if (!tokenCall.reverted) {
+    return tokenCall.value
+  }
+
+  log.warning('token() call reverted for {}', [address.toHexString()])
+  return ZERO_ADDRESS
 }
 
 function getTransactionId(event: ethereum.Event): string {
