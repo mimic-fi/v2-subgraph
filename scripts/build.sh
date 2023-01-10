400f04a52aa59e008b881def871735a4459b4ee1
Facu Spagnuolo <facundo_spagnuolo@icloud.com>
2023-01-10 20:27:59 -0300

    handlers: support smart vaults factory

--- scripts/build.sh
+++ scripts/build.sh
@@ -4,19 +4,23 @@
 set -o errexit
 
 # Mainnet
-registry_mainnet=0x53D627B1a2993139b32d5dF209A94498d691f21A
+old_registry_mainnet=0x53D627B1a2993139b32d5dF209A94498d691f21A
+smart_vaults_factory_mainnet=0x8373c68629191EF10f654CE8e32bbfe3c7A1D743
 block_mainnet=15950104
 
 # Polygon
-registry_matic=0x53D627B1a2993139b32d5dF209A94498d691f21A
+old_registry_matic=0x53D627B1a2993139b32d5dF209A94498d691f21A
+smart_vaults_factory_matic=0x8373c68629191EF10f654CE8e32bbfe3c7A1D743
 block_matic=
 
 # Goerli
-registry_goerli=0x53D627B1a2993139b32d5dF209A94498d691f21A
+old_registry_goerli=0x53D627B1a2993139b32d5dF209A94498d691f21A
+smart_vaults_factory_goerli=0x8373c68629191EF10f654CE8e32bbfe3c7A1D743
 block_goerli=7936627
 
 # Mumbai
-registry_mumbai=0x53D627B1a2993139b32d5dF209A94498d691f21A
+old_registry_mumbai=0x53D627B1a2993139b32d5dF209A94498d691f21A
+smart_vaults_factory_mumbai=0x8373c68629191EF10f654CE8e32bbfe3c7A1D743
 block_mumbai=29929062
 
 # Validate network
@@ -44,13 +48,25 @@ fi
 
 # Load registry address
 if [[ -z $REGISTRY_ADDRESS ]]; then
-  REGISTRY_ADDRESS_VAR=registry_$NETWORK
+  REGISTRY_ADDRESS_VAR=old_registry_$NETWORK
   REGISTRY_ADDRESS=${!REGISTRY_ADDRESS_VAR}
 fi
 
 # Validate registry address
 if [[ -z $REGISTRY_ADDRESS ]]; then
   echo 'Please make sure a Registry address is provided'
+  exit 1
+fi
+
+# Load smart vaults factory address
+if [[ -z $SMART_VAULS_FACTORY_ADDRESS ]]; then
+  SMART_VAULS_FACTORY_ADDRESS_VAR=smart_vaults_factory_$NETWORK
+  SMART_VAULS_FACTORY_ADDRESS=${!SMART_VAULS_FACTORY_ADDRESS_VAR}
+fi
+
+# Validate smart vaults factory address
+if [[ -z $SMART_VAULS_FACTORY_ADDRESS ]]; then
+  echo 'Please make sure a Smart Vaults Factory address is provided'
   exit 1
 fi
 
@@ -65,10 +81,11 @@ if [ -f subgraph.yaml ]; then
 fi
 
 # Build subgraph manifest for requested variables
-echo "Preparing new subgraph manifest for Registry address ${REGISTRY_ADDRESS} and network ${NETWORK}"
+echo "Preparing new subgraph manifest for network ${NETWORK}"
 cp subgraph.template.yaml subgraph.yaml
 sed -i -e "s/{{network}}/${ENV}/g" subgraph.yaml
 sed -i -e "s/{{registryAddress}}/${REGISTRY_ADDRESS}/g" subgraph.yaml
+sed -i -e "s/{{smartVaultsFactoryAddress}}/${SMART_VAULS_FACTORY_ADDRESS}/g" subgraph.yaml
 sed -i -e "s/{{blockNumber}}/${BLOCK_NUMBER}/g" subgraph.yaml
 rm -f subgraph.yaml-e
 
